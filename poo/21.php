11ced9eed1fda9e5554723891a85d280b9c3a515
planetacomputer <evapardeza123@gmail.com>
2016-05-30 21:10:28 +0200

    POO Usuarios Bahit

--- poo/21.php
+++ poo/21.php
@@ -6,28 +6,37 @@ class Banner {
   private $imagen;
   private $colorTexto;
   private $colorFondo;
+
   public function __construct($an,$al,$men)
   {
     $this->ancho=$an;
     $this->alto=$al;
     $this->mensaje=$men;
-    $this->imagen=imagecreate($this->ancho,$this->alto);
+    $this->imagen=imagecreate($this->ancho, $this->alto);
     $this->colorTexto=imagecolorallocate($this->imagen,255,255,0);
     $this->colorFondo=imagecolorallocate($this->imagen,255,0,0);
-    imagefill($this->imagen,0,0,$this->colorFondo);
+    
   }
   public function graficar()
   {
-    imagestring ($this->imagen,50,50,10, $this->mensaje,$this->colorFuente);
-    header ("Content-type: image/png");
+    imagefill($this->imagen,0,0,$this->colorFondo);
+    imagestring($this->imagen, 4, 30, 25, $this->mensaje, $this->colorTexto);
+    imagesetthickness($this->imagen, 5);
+    imageline($this->imagen, 30, 45, 165, 45, $this->colorTexto);
+    header( "Content-type: image/png" );
     imagepng($this->imagen);
+    imagecolordeallocate($this->colorTexto);
+    imagecolordeallocate($this->colorFondo);
   }
   public function __destruct()
   {
     imagedestroy($this->imagen);
   }
+
 }
 
-$baner1=new Banner(428,45,'Sistema de Ventas por Mayor y Menor');
+$baner1 = new Banner(628, 145,'Sistema de Ventas');
 $baner1->graficar();
-?> 
+
+
+?>
