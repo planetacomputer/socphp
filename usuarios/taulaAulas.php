5c2c825993ca6f4050419f8b3b4bb5e6f8900cad
planetacomputer <evapardeza123@gmail.com>
2016-05-31 21:08:48 +0200

    Final

--- usuarios/taulaAulas.php
+++ usuarios/taulaAulas.php
@@ -6,13 +6,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Taula de gestio d'Aules</title>
+	<meta charset="UTF-8">
+	<title>Taula de gestió d'Aules</title>
 	<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
+	<style>
+		.w3-lobster {
+		  font-family: "Lobster", serif;
+		}
+	</style>
 </head>
 <body>
 <table class="w3-table-all w3-hoverable">
-	  <caption>Gestió d'aules</caption>
-	  <tr class="w3-red">
+	  <caption><h1 class="w3-lobster w3-amber w3-text-brown">Gestió d'aules</h1></caption>
+	  <tr class="w3-orange">
 	    <th>Id</th>
 	    <th>Numero</th> 
 	    <th>Superficie</th>
@@ -24,8 +31,10 @@
 			<td><?php echo $row['num']; ?></td>
 			<td><?php echo $row['superficie']; ?></td>
 			<td><?php echo $row['edificio']; ?></td>
-			<td>Modificar</td>
-			<td><a href="eliminar.php?num=<?php echo $row['num']?>">Eliminar</td>
+			<td><a class="w3-btn-block w3-khaki w3-round w3-ripple" href="modificar.php?num=<?php echo $row['num']?>">Modificar</a>
+			</td>
+			<td><a class="w3-btn-block w3-red w3-round w3-ripple" href="eliminar.php?num=<?php echo $row['num']?>">Eliminar</a>
+			</td>
 		</tr>
 	<?php endforeach ?>
 </table>
