9c11600adf648c64307bd70f7316df643f2811b8
planetacomputer <evapardeza123@gmail.com>
2016-11-04 19:25:03 +0100

    Nuevas funcionalidades del generador, como numPremio y maxIntentos

--- twig/numbers.php
+++ twig/numbers.php
@@ -4,6 +4,9 @@ session_start();
 // include and register Twig auto-loader
 include 'Twig/Autoloader.php';
 Twig_Autoloader::register();
+$maxIntentos = 300;
+$num = rand (0,3);
+$numPremio = 1;
 
 try {
   // define template directory location
@@ -15,9 +18,10 @@ try {
   // load template
   $template = $twig->loadTemplate('numbers.tmpl');
 
+  
   // generate random number and
   // check whether odd or even
-  $num = rand (0,30);
+  
   if(!isset($_SESSION["numbers"])){
     $arrNumbers = array(); 
   }
@@ -28,13 +32,17 @@ try {
   $_SESSION["numbers"]  = $arrNumbers;
   
   $div = ($num % 2);
+  $activado = ($maxIntentos > $arrNumbers.length) && ($num != $numPremio);
+  echo $activado;
 
   // set template variables
   // render template
   echo $template->render(array (
     'num' => $num,
     'div' => $div,
-    'numbers' => array_reverse($_SESSION["numbers"])
+    'numbers' => array_reverse($_SESSION["numbers"]),
+    'activado' => $activado,
+    'numPremio' => $numPremio
   ));
   
 } catch (Exception $e) {
